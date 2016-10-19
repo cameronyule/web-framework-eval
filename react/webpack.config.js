8a1be6f2df6bd51846368db181a520dfefc60c13
Cameron Yule <cameron@cameronyule.com>
2016-10-19 13:30:16 +0100

    Integrated code linting with eslint

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,13 +1,28 @@
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: "./bin",
-    filename: "bundle.js",
-    publicPath: "/assets/"
+    path: './bin',
+    filename: 'bundle.js',
+    publicPath: '/assets/'
   },
   module: {
-	  loaders: [
-	    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-	  ]
-  }
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['eslint'],
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  eslint: {
+    failOnWarning: true,
+    failOnError: true
+  },
 };
